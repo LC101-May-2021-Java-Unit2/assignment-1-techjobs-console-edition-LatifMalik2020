55b867df086d572c05516ebda4d9710ade18c2f5
LatifMalik2020 <78774688+LatifMalik2020@users.noreply.github.com>
2021-09-12 20:29:55 -0500

    try again 4

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -78,8 +78,7 @@ public class JobData {
         for (HashMap<String, String> row : allJobs) {
 
             String aValue = row.get(column).toLowerCase();;
-
-            if (aValue.contains(value)) {
+            if (aValue.contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
